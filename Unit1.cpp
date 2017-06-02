bae5789354bf6c2d180ba5bbc78316466e860607
SashaPrist <SashaPrist1998@gmail.com>
2017-06-02 21:29:28 +0300

    Add files via upload

---
+++ Unit1.cpp
@@ -0,0 +1,173 @@
+﻿//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+#include "Unit2.h"
+#include <fstream>
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+Projects A[20];
+int i = 0;
+int r = 0;
+char Temp[20];
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+Memo1->Clear();
+ifstream in("C:\\Users\\PC\\Desktop\\Laba2.txt");
+string s;
+while (in.peek() != EOF)
+{
+	getline(in, s);
+	r ++;
+}
+in.close();
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+FILE *file;
+file = fopen("C:\\Users\\PC\\Desktop\\Laba2.txt","a+");
+AnsiString Name = Form1->Edit1->Text;
+
+AnsiString Task = Form1->Edit2->Text;
+
+AnsiString NameExec = Form1->Edit3->Text;
+
+AnsiString NameLider = Form1->Edit4->Text;
+
+int day1, month1, year1, day2, month2, year2;
+int Day1, Month1, Year1, Day2, Month2, Year2;
+strcpy(A[i].Name, Name.c_str());
+strcpy(A[i].Task, Task.c_str());
+strcpy(A[i].NameExec, NameExec.c_str());
+strcpy(A[i].NameLider, NameLider.c_str());
+
+fputs(A[i].Name,file);
+fputs("\n",file);
+
+fputs(A[i].Task,file);
+fputs("\n",file);
+
+fputs(A[i].NameExec,file);
+fputs("\n",file);
+
+fputs(A[i].NameLider,file);
+fputs("\n",file);
+
+day1 = StrToInt(Edit5->Text);
+Day1 = 1;
+fprintf(file,"%d", day1);
+fputs("\n",file);
+
+month1 = StrToInt(Edit6->Text);
+Month1 = month1;
+fprintf(file,"%d", month1);
+fputs("\n",file);
+
+year1 = StrToInt(Edit7->Text);
+Year1 = year1;
+fprintf(file,"%d", year1);
+fputs("\n",file);
+
+day2 = StrToInt(Edit8->Text);
+Day2 = day2;
+fprintf(file,"%d", day2);
+fputs("\n",file);
+
+month2 = StrToInt(Edit9->Text);
+Month2 = month2;
+fprintf(file,"%d", month2);
+fputs("\n",file);
+
+year2 = StrToInt(Edit10->Text);
+Year2 = year2;
+fprintf(file,"%d", year2);
+fputs("\n",file);
+Memo1->Lines->Add(A[i].Name);
+Memo1->Lines->Add(A[i].Task);
+Memo1->Lines->Add(A[i].NameExec);
+Memo1->Lines->Add(A[i].NameLider);
+Memo1->Lines->Add(day1);
+Memo1->Lines->Add(month1);
+Memo1->Lines->Add(year1);
+Memo1->Lines->Add(day2);
+Memo1->Lines->Add(month2);
+Memo1->Lines->Add(year2);
+A[i].GetInformation(file,A[i].Name, A[i].Task, A[i].NameExec, A[i].NameLider, year1, month1, day1, year2, month2, day2);
+i ++;
+fclose(file);
+}
+//---------------------------------------------------------------------------
+void a(TMemo *Memo){
+
+}
+void __fastcall TForm1::Button3Click(TObject *Sender)
+{
+	Memo1->Clear();
+	AnsiString temp = Form1->Edit11->Text;
+	strcpy(Temp, temp.c_str());
+
+	for(int j = 0; j < i; j++)
+	{
+		A[j].Print(Temp, Memo1);
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button4Click(TObject *Sender)
+{
+	Memo1->Lines->Clear();
+	for(int j = 0; j < i; j++)
+	{
+		A[j].Clock(Memo1);
+	}
+}
+//---------------------------------------------------------------------------
+
+
+
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+	FILE *file2;
+	Memo1->Clear();
+	i += (r / 10);
+	file2 = fopen("C:\\Users\\PC\\Desktop\\Laba2.txt","r+");
+	for(int j = 0; j < i; j++)
+		A[j].Read(file2, Memo1);
+}
+//---------------------------------------------------------------------------
+
+
+
+void __fastcall TForm1::Button5Click(TObject *Sender)
+{
+	Memo1->Clear();
+	AnsiString temp = Form1->Edit12->Text;
+	strcpy(Temp, temp.c_str());
+	for(int j=0; j < i; j++)
+	{
+		A[j].PrintLiderProjets(Temp,Memo1);
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button6Click(TObject *Sender)
+{
+	Memo1->Clear();
+	AnsiString temp = Form1->Edit13->Text;
+	strcpy(Temp, temp.c_str());
+	for(int j=0; j < i; j++)
+	{
+		A[j].PrintProjeсtExec(Temp, Memo1);
+	}
+
+}
+//---------------------------------------------------------------------------
+
---
+++ Unit2.h
@@ -0,0 +1,177 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef Unit2H
+#define Unit2H
+//---------------------------------------------------------------------------
+#endif
+#include <vcl.h>
+#include "math.h"
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int fun (int,int,int,int);
+
+class Projects{
+	public :
+	char Name[20];
+	char Task[20];
+	char NameExec[20];
+	char NameLider[20];
+	int year1;
+	int month1;
+	int day1;
+	int year2;
+	int month2;
+	int day2;
+	int Time;
+
+	void GetInformation(FILE *fp, char *name, char *task, char *nameexec, char *namelider, int year1, int month1, int day1, int year2, int month2, int day2){
+
+	strcpy(Name, name);
+	strcpy(Task, task);
+	strcpy(NameExec, nameexec);
+	strcpy(NameLider, namelider);
+	this->year1 = year1;
+	this->month1 = month1;
+	this->day1 = day1;
+	this->year2 = year2;
+	this->month2 = month2;
+	this->day2 = day2;
+	Timer(day1, month1, year1, day2, month2, year2);
+	}
+
+	void Print(char *temp, TMemo *Memo1){
+		if(strcmp(temp, NameLider) == 0)
+		{
+			//Memo1->Lines->Add("Работает");
+			Memo1->Lines->Add(AnsiString("Название: ") + Name);
+			Memo1->Lines->Add(AnsiString("Задание: ") + Task);
+			Memo1->Lines->Add(AnsiString("Сотрудник: ") + NameExec);
+			Memo1->Lines->Add(AnsiString("Руководитель: ") + NameLider);
+			Memo1->Lines->Add(AnsiString("Дата начала: ") + AnsiString(day1) + " "+AnsiString(month1) + " " + AnsiString(year1));
+			Memo1->Lines->Add(AnsiString("Дата конца: ") + AnsiString(day2) + " "+AnsiString(month2) + " " + AnsiString(year2));
+		}
+	}
+	void PrintLiderProjets (char *temp, TMemo *Memo1)
+	{
+		if(strcmp(temp, NameExec) == 0)
+			Memo1->Lines->Add(AnsiString("Задание: ") + Task);
+
+	}
+	void PrintProjeсtExec (char *temp, TMemo *Memo1)
+	{
+		if(strcmp(temp, Name) == 0)
+			Memo1->Lines->Add(AnsiString("Сотрудник: ") + NameExec);
+	}
+	void Read(FILE *fp,TMemo *Memo1){
+
+		fscanf(fp,"%s",Name);
+		fscanf(fp,"%s",Task);
+		fscanf(fp,"%s",NameExec);
+		fscanf(fp,"%s",NameLider);
+
+		fscanf(fp,"%d",&day1);
+		fscanf(fp,"%d",&month1);
+		fscanf(fp,"%d",&year1);
+		fscanf(fp,"%d",&day2);
+		fscanf(fp,"%d",&month2);
+		fscanf(fp,"%d",&year2);
+		Time = Timer(day1, month1, year1, day2, month2, year2);
+
+		Memo1->Lines->Add(AnsiString("Название:") + Name);
+		Memo1->Lines->Add(AnsiString("Задание:") + Task);
+		Memo1->Lines->Add(AnsiString("Сотрудник:") + NameExec);
+		Memo1->Lines->Add(AnsiString("Руководитель:") + NameLider);
+		Memo1->Lines->Add(AnsiString("Дата начала:") + AnsiString(day1) + " "+AnsiString(month1) + " " + AnsiString(year1));
+		Memo1->Lines->Add(AnsiString("Дата конца:") + AnsiString(day2) + " "+AnsiString(month2) + " " + AnsiString(year2));
+	}
+
+	int Timer(int day1, int month1, int year1, int day2, int month2, int year2)
+	{
+		int i, day, day0, year, daymonth=0;
+		year = year2 - year1;
+		day0 = year / 4;
+		day = year * 365;
+		Time = year * 365;
+		day += day0;
+		Time += day0;
+		daymonth = fun (month1, daymonth, day1, year1);
+		day -= daymonth;
+		Time -= daymonth;
+		daymonth = 0;
+		daymonth = fun (month2, daymonth, day2, year2);
+		day += daymonth;
+		Time += daymonth;
+		return Time;
+	}
+
+	void Clock(TMemo *Memo1){
+	if(Time > 31)
+	{
+		Memo1->Lines->Add("Работает 2");
+		Memo1->Lines->Add(AnsiString("Название:") + Name);
+		Memo1->Lines->Add(AnsiString("Задание:") + Task);
+		Memo1->Lines->Add(AnsiString("Сотрудник:") + NameExec);
+		Memo1->Lines->Add(AnsiString("Руководитель:") + NameLider);
+		Memo1->Lines->Add(AnsiString("Дата начала:") + AnsiString(day1) + " " + AnsiString(month1) + " " + AnsiString(year1));
+		Memo1->Lines->Add(AnsiString("Дата конца:") + AnsiString(day2) + " " + AnsiString(month2) + " " + AnsiString(year2));
+		Memo1->Lines->Add(AnsiString("Время выполнения") + Time);
+	}
+}
+	int fun (int month1, int daymonth, int day1, int year)
+	{
+	switch(month1)
+	{
+	case 1:
+		daymonth = 0 + day1;
+		break;
+	case 2:
+		daymonth = 31 + day1;
+		break;
+	case 3:
+		if (year - (year / 4) * 4 == 0)daymonth = day1 + 60;
+		else daymonth = 59 + day1;
+		break;
+	case 4:
+		if (year - (year / 4) * 4 == 0)daymonth = 91 + day1;
+		else daymonth = 90 + day1;
+		break;
+	case 5:
+		if (year -(year / 4) * 4 == 0) daymonth = 121 + day1;
+		else daymonth = 120 + day1;
+		break;
+	case 6:
+		if (year - (year / 4) * 4 == 0) daymonth = 152 + day1;
+		else daymonth = 151 + day1;
+		break;
+	case 7:
+		if (year - (year / 4) * 4 == 0) daymonth = 182 + day1;
+		else daymonth=181+day1;
+		break;
+	case 8:
+		if (year - (year / 4) * 4 == 0) daymonth = 213 + day1;
+		else daymonth = 212 + day1;
+		break;
+	case 9:
+		if (year - (year / 4) * 4 == 0) daymonth = 244 + day1;
+		else daymonth = 243 + day1;
+		break;
+	case 10:
+		if (year - (year / 4) * 4 == 0) daymonth = 274 + day1;
+		else daymonth = 273 + day1;
+		break;
+	case 11:
+		if (year - (year / 4) * 4 == 0) daymonth = 305 + day1;
+		else daymonth = 304 + day1;
+		break;
+	case 12:
+		if (year - (year / 4) * 4 == 0) daymonth = 335 + day1;
+		else daymonth = 334 + day1;
+		break;
+	}
+	return daymonth;
+	}
+};
